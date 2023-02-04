86687182480a90b7473f31a0d55f63b5e5eb61f6
ppopow <pawel.popow@gmail.com>
2023-02-04 11:19:38 +0000

    fife commit

--- games/pong.py
+++ games/pong.py
@@ -7,24 +7,24 @@ from pygame import surface
 
 class Board(object):
     """
-       Plansza do gry. Odpowiada za rysowanie okna gry.
+    Plansza do gry. Odpowiada za rysowanie okna gry.
     """
 
     def __init__(self, width, height):
         """
-            Konstruktor planszy do gry. Przygotowuje okienko gry.
+        Konstruktor planszy do gry. Przygotowuje okienko gry.
 
-            :param width:
-            :param height:
+        :param width:
+        :param height:
         """
         self.surface = pygame.display.set_mode((width, height), 0, 32)
         pygame.display.set_caption('Simple Pong')
 
     def draw(self, *args):
         """
-            Rysuje okno gry
+        Rysuje okno gry
 
-            :param args: lista obiektów do narysowania
+        :param args: lista obiektów do narysowania
         """
         background = (230, 255, 255)
         self.surface.fill(background)
@@ -36,12 +36,9 @@ class Board(object):
         pygame.display.update()
 
 
-time.sleep(5)
-
-
 class PongGame(object):
     """
-        Łączy wszystkie elementy gry w całość.
+    Łączy wszystkie elementy gry w całość.
     """
 
     def __init__(self, width, height):
@@ -53,7 +50,7 @@ class PongGame(object):
 
     def run(self):
         """
-            Główna pętla programu
+        Główna pętla programu
         """
         while not self.handle_events():
             # działaj w pętli do momentu otrzymania sygnału do wyjścia
@@ -62,9 +59,9 @@ class PongGame(object):
 
     def handle_events(self):
         """
-            Obsługa zdarzeń systemowych, tutaj zinterpretujemy np. ruchy myszką
+        Obsługa zdarzeń systemowych, tutaj zinterpretujemy np. ruchy myszką
 
-            :return True jeżeli pygame przekazał zdarzenie wyjścia z gry
+        :return True jeżeli pygame przekazał zdarzenie wyjścia z gry
         """
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
@@ -77,7 +74,7 @@ class PongGame(object):
 
 class Drawable(object):
     """
-        Klasa bazowa dla rysowanych obiektów
+    Klasa bazowa dla rysowanych obiektów
     """
 
     def __init__(self, width, height, x, y, color=(0, 255, 0)):
@@ -87,13 +84,13 @@ class Drawable(object):
         self.surface = pygame.Surface([width, height], pygame.SRCALPHA, 32).convert_alpha()
         self.rect = self.surface.get_rect(x=x, y=y)
 
-    def draw_on(self):
+    def draw_on(self, surface):
         surface.blit(self.surface, self.rect)
 
 
 class Ball(Drawable):
     """
-        Piłeczka, sama kontroluje swoją prędkość i kierunek poruszania się.
+    Piłeczka, sama kontroluje swoją prędkość i kierunek poruszania się.
     """
 
     def __init__(self, width, height, x, y, color=(255, 0, 0), x_speed=3, y_speed=3):
@@ -106,29 +103,66 @@ class Ball(Drawable):
 
     def bounce_y(self):
         """
-            Odwraca wektor prędkości w osi Y
+        Odwraca wektor prędkości w osi Y
         """
         self.y_speed *= -1
 
     def bounce_x(self):
         """
-            Odwraca wektor prędkości w osi X
+        Odwraca wektor prędkości w osi X
         """
         self.x_speed *= -1
 
     def reset(self):
         """
-            Ustawia piłeczkę w położeniu początkowym i odwraca wektor prędkości w osi Y
+        Ustawia piłeczkę w położeniu początkowym i odwraca wektor prędkości w osi Y
         """
         self.rect.move(self.start_x, self.start_y)
         self.bounce_y()
 
     def move(self):
         """
-            Przesuwa piłeczkę o wektor prędkości
+        Przesuwa piłeczkę o wektor prędkości
         """
         self.rect.x += self.x_speed
         self.rect.y += self.y_speed
+
+
+class PongGame(object):
+    """
+    Łączy wszystkie elementy gry w całość.
+    """
+
+    def __init__(self, width, height):
+        pygame.init()
+        self.board = Board(width, height)
+        # zegar którego użyjemy do kontrolowania szybkości rysowania
+        # kolejnych klatek gry
+        self.fps_clock = pygame.time.Clock()
+        self.ball = Ball(20, 20, width / 2, height / 2)
+
+    def run(self):
+        """
+        Główna pętla programu
+        """
+        while not self.handle_events():
+            # działaj w pętli do momentu otrzymania sygnału do wyjścia
+            self.ball.move()
+            self.board.draw(
+                self.ball,
+            )
+            self.fps_clock.tick(30)
+
+    def handle_events(self):
+        """
+               Obsługa zdarzeń systemowych, tutaj zinterpretujemy np. ruchy myszką
+
+               :return True jeżeli pygame przekazał zdarzenie wyjścia z gry
+               """
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                return True
 
 
 if __name__ == '__main__':
